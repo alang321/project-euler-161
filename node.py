c9526ae939c9d713bf970534adfb71aa16fd4f5c
alang321 <oesterreich25euro@gmail.com>
2020-12-31 16:07:00 +0100

    program can now find single solution and draw it

--- node.py
+++ node.py
@@ -1,6 +1,8 @@
 class Node:
 
-    def __init__(self, val, prev_h=None, next_h=None, prev_v=None, next_v=None):
+    def __init__(self, val=None, prev_h=None, next_h=None, prev_v=None, next_v=None, isColHeader=False, isOrigin=False):
+        self.isColHeader = isColHeader
+        self.isOrigin = isOrigin
         self.val = val
         self.prev_h = prev_h
         self.prev_v = prev_v
@@ -8,3 +10,11 @@ class Node:
         self.next_v = next_v
         self.adjac_h = [prev_h, next_h]
         self.adjac_v = [prev_v, next_v]
+
+    def __iter__(self):
+        self.prev = self
+        return self
+
+    def __next__(self):
+        self.prev = self.prev.next_h
+        return self.prev
--- solution-161.py
+++ solution-161.py
@@ -1,4 +1,7 @@
 from node import Node
+import matplotlib.pyplot as plt
+import numpy as np
+
 #dancing link with backtracking (i think) for euler problem 161
 
 triominoes = [[[0, 0], [0, 1], [1, 0]],
@@ -38,8 +41,8 @@ def getPossibleTriominoePlacements(grid, polyominoes):
 
     return indexList
 
-def getDancingLinkList(grid, triominoes):
-    triomineFieldsList = getPossibleTriominoePlacements(grid, triominoes)
+def getDancingLinkList(grid, triominoePlacements):
+    triomineFieldsList = triominoePlacements
 
     width = len(grid[0])
     height = len(grid)
@@ -57,9 +60,11 @@ def getDancingLinkList(grid, triominoes):
     # generate the dancing link array as double linked both col and row
     dancingLinkList = []
 
-    previousNodeCol = [None] * len(validFields)
+    # initialize column headers, these dont hold the amount of 1's, just the column id
+    colHeaders = [Node(col, isColHeader=True) for col in range(len(validFields))]
+    linkHorizontalCircular([Node(isOrigin=True), *colHeaders])
 
-    firstNodeCol = [None] * len(validFields)
+    previousNodeCol = colHeaders.copy()
 
     for rowIndex, triomineFields in enumerate(triomineFieldsList):
         doubleLinkNodesRow = []
@@ -69,58 +74,176 @@ def getDancingLinkList(grid, triominoes):
             node = Node([rowIndex, colIndex])
 
             #vertical
-            if firstNodeCol[colIndex] is None: # if its the first 1 in this column
-                firstNodeCol[colIndex] = node
-            else:
-                node.prev_v = previousNodeCol[colIndex]
-                node.prev_v.next_v = node
+            node.prev_v = previousNodeCol[colIndex]
+            node.prev_v.next_v = node
 
             previousNodeCol[colIndex] = node
 
             doubleLinkNodesRow.append(node)
 
         #horizontal double link
-        for nodeIndex in range(len(doubleLinkNodesRow)):
-            prev = (nodeIndex - 1) % len(doubleLinkNodesRow)
-            next = (nodeIndex + 1) % len(doubleLinkNodesRow)
-
-            doubleLinkNodesRow[nodeIndex].prev_h = doubleLinkNodesRow[prev]
-            doubleLinkNodesRow[nodeIndex].next_h = doubleLinkNodesRow[next]
+        linkHorizontalCircular(doubleLinkNodesRow)
 
         dancingLinkList.extend(doubleLinkNodesRow)
 
-    for colIndex, node in enumerate(firstNodeCol):
+    for colIndex, node in enumerate(colHeaders):
         node.prev_v = previousNodeCol[colIndex]
         previousNodeCol[colIndex].next_v = node
 
-    return dancingLinkList
+    return dancingLinkList, colHeaders, colHeaders[0].prev_h
 
-def algorithmX(dancingLinkList):
+def linkHorizontalCircular(nodeList):
+    for nodeIndex in range(len(nodeList)):
+        prev = (nodeIndex - 1) % len(nodeList)
+        next = (nodeIndex + 1) % len(nodeList)
+
+        nodeList[nodeIndex].prev_h = nodeList[prev]
+        nodeList[nodeIndex].next_h = nodeList[next]
+
+def algorithmX(nodeOrigin):
+    partialSolution = []
+    while True:
+        print("LOOP:")
+        print(getMatrixString(nodeOrigin, colHeaders, linkedList))
+        #if empty, so solution is found
+        if nodeOrigin.next_h is nodeOrigin:
+            print("array has no more columns")
+            print([i.val[0] for i in partialSolution])
+            break
+        else:
+            invalid = False
+            for node in origin:
+                if node is origin:
+                    break
+                if node.next_v is node:
+                    invalid = True
+                    break
+            if invalid:
+                print("Combination:")
+                print(partialSolution)
+                print("does not work")
+        col = nodeOrigin.next_h
+        row = col.next_v
+
+        partialSolution.append(row)
+
+        # go through columns that have a 1 in this row and delete
+        lastCol = row.prev_h
+        currentCol = row
+        while True:
+            #go through rows that have a 1 in this column and delete
+            lastRow = currentCol.prev_v
+            currentRow = currentCol
+            while True:
+                removeRow(currentRow)
+                if currentRow is lastRow:
+                    break
+                currentRow = currentRow.next_v
+
+            removeCol(currentCol)
+            if currentCol is lastCol:
+                break
+            currentCol = currentCol.next_h
+    return partialSolution
+
+def removeRow(node):
+    if node.isColHeader:
+        return
+    initial = node
+    current = node
+
+    while True:
+        current.prev_v.next_v = current.next_v
+        current.next_v.prev_v = current.prev_v
+
+        current = current.next_h
+        if initial is current:
+            break
     return
 
-def removeRow():
+def removeCol(node):
+    initial = node.prev_v
+    current = node.prev_v
+
+    while True:
+        current.prev_h.next_h = current.next_h
+        current.next_h.prev_h = current.prev_h
+
+        current = current.next_v
+        if initial is current:
+            break
     return
 
-def removeCol():
-    return
+def getMatrixString(origin, colHeaders, linkedList):
+    width = len(colHeaders)
 
+    height = 0
+    for node in linkedList:
+        height = max(node.val[0], height)
+    height += 1
+
+    consideredRows = [0] * height
+    for node in origin:
+        if node is origin:
+            break
+
+        while True:
+            node = node.next_v
+            if node.isColHeader:
+                break
+            consideredRows[node.val[0]] = 1
+
+    consideredColumns = [0] * width
+    for node in origin:
+        if node is origin:
+            break
+        consideredColumns[node.val] = 1
+
+    display = [[0] * height for _ in range(width)]
+    string = ""
+
+    for node in linkedList:
+        display[node.val[1]][node.val[0]] = 1
+
+
+    for i in range(height):
+        if consideredRows[i] == 1:
+            for j in range(width):
+                if consideredColumns[j] == 1:
+                    string += str(display[j][i]) + " "
+            string += "\n"
+    return string
+
+def drawSolution(grid, triominoeList):
+    # Make a 9x9 grid...
+    nrows, ncols = len(grid), len(grid[1])
+    image = np.zeros(nrows * ncols)
+
+    # Set every other cell to a random number (this would be your data)
+    for index, triominoe in enumerate(triominoeList):
+        for point in triominoe:
+            image[point[0] * ncols + point[1]] = index
+
+    # Reshape things into a 9x9 grid.
+    image = image.reshape((nrows, ncols))
+
+    row_labels = range(nrows)
+    col_labels = range(ncols)
+    plt.matshow(image)
+    plt.xticks(range(ncols), col_labels)
+    plt.yticks(range(nrows), row_labels)
+    plt.show()
 
 grid = getGeneratedGrid([9, 2])
 
-print(getPossibleTriominoePlacements(grid, triominoes))
+triominoePlacements = getPossibleTriominoePlacements(grid, triominoes)
 
-a = getDancingLinkList(grid, triominoes)
+linkedList, colHeaders, origin = getDancingLinkList(grid, triominoePlacements)
 
-display = [[0] * 46 for _ in range(18)]
-string = ""
+sol = algorithmX(origin)
 
+triominoeList = []
+for i in sol:
+    triominoeList.append(triominoePlacements[i.val[0]])
 
-for node in a:
-    display[node.val[1]][node.val[0]] = 1
-
-for i in range(46):
-    for j in range(18):
-        string += str(display[j][i]) + " "
-    string += "\n"
-
-print(string)
+drawSolution(grid, triominoeList)
